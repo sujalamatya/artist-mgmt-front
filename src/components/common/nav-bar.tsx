d18ed207bf1144cf4625072e19a96fbed10f76ba
sujalamatya <sujalcr10@gmail.com>
2025-03-18 13:29:56 +0545

    feat(theme): add theme switcher

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Avatar } from "../ui/avatar";
 import {
@@ -6,34 +8,39 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { User, LogOut, Settings, HelpCircle, Bell, Mail } from "lucide-react"; // Import additional Lucide icons
+import { User, LogOut, Settings, HelpCircle, Bell, Mail } from "lucide-react"; // Import Lucide icons
+import { ThemeToggle } from "../theme-toggle";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-end p-2 right-0">
+    <div className="flex items-center justify-end gap-4 p-2 right-0">
+      {/* Profile Dropdown */}
+      <ThemeToggle />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <button className="focus:outline-none">
             <Avatar className="h-10 w-10 cursor-pointer border-2 border-white/20 hover:border-white/50 transition-colors bg-amber-600" />
           </button>
         </DropdownMenuTrigger>
+
         <DropdownMenuContent
           align="end"
-          className="w-48 rounded-lg bg-white/90 backdrop-blur-md shadow-lg border border-white/20"
+          className="w-48 rounded-lg bg-white dark:bg-gray-900 backdrop-blur-md shadow-lg border border-white/20"
         >
           {/* Profile */}
-          <DropdownMenuItem className="p-2 hover:bg-gray-100/50 rounded-md cursor-pointer">
+          <DropdownMenuItem className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md cursor-pointer">
             <User className="mr-2 h-4 w-4" />
             <span>Profile</span>
           </DropdownMenuItem>
+
           {/* Help */}
-          <DropdownMenuItem className="p-2 hover:bg-gray-100/50 rounded-md cursor-pointer">
+          <DropdownMenuItem className="flex items-center p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md cursor-pointer">
             <HelpCircle className="mr-2 h-4 w-4" />
             <span>Help</span>
           </DropdownMenuItem>
 
           {/* Logout */}
-          <DropdownMenuItem className="p-2 hover:bg-gray-100/50 rounded-md cursor-pointer">
+          <DropdownMenuItem className="flex items-center p-2 hover:bg-red-100 dark:hover:bg-red-900 rounded-md cursor-pointer text-red-600 dark:text-red-400">
             <LogOut className="mr-2 h-4 w-4" />
             <span>Logout</span>
           </DropdownMenuItem>
