a051afc68b3e64aa40339eea5c87b5be5865b4a6
sujalamatya <sujalcr10@gmail.com>
2025-03-18 14:28:06 +0545

    feat: images slide UI

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/common/app-sidebar";
 import Navbar from "@/components/common/nav-bar";
+import ImageCarousel from "@/components/main-pics";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -45,7 +46,7 @@ export default function Page() {
             </div>
           </header>
 
-          {/* Main content */}
+          {/* Main content
           <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             <div className="grid auto-rows-min gap-4 md:grid-cols-3">
               <div className="bg-muted/50 aspect-video rounded-xl" />
@@ -53,7 +54,8 @@ export default function Page() {
               <div className="bg-muted/50 aspect-video rounded-xl" />
             </div>
             <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-          </div>
+          </div> */}
+          <ImageCarousel />
         </SidebarInset>
       </SidebarProvider>
     </div>
