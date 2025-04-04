0a698faf361dceb942b6b8bd59b8ba57a7b7dc1d
sujalamatya <sujalcr10@gmail.com>
2025-04-04 16:51:49 +0545

    chore: minor refactor

---
+++ src/features/artist/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as ArtistTable } from "./artist-table";
+export { default as ViewArtist } from "./artist-profile";
+export { default as ArtistProfileSongs } from "./artist-profile-songs";
+export { default as EditArtist } from "./artist-edit-table";
+export { default as AddArtist } from "./artist-add";
